bebd04ba280158e54c8e90cd6dbe5bd93403e208
Jessica-Thomas <jessica.thomas648@gmail.com>
2021-06-26 13:12:37 -0400

    2 songs added

---
+++ MadLib/Regulate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MadLib
+{
+    public class Regulate
+    {
+        public Regulate()
+        {
+            Console.WriteLine("Please give me a plural noun:  ");
+            string pluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Please give me a color:  ");
+            string Color = Console.ReadLine();
+
+            Console.WriteLine("Please give me another color:  ");
+            string secondColor = Console.ReadLine();
+
+            Console.WriteLine("Please give me a noun:  ");
+            string Noun = Console.ReadLine();
+
+            Console.WriteLine("Please give me a time of day:  ");
+            string timeOfDay = Console.ReadLine();
+
+            Console.WriteLine("Please give me a place:  ");
+            string Place = Console.ReadLine();
+
+
+            string madLibRegulators = pluralNoun + " , mount up... \n " +
+                                            "It was a clear " + Color + " night, a clear " + secondColor + " moon. \n" +
+                                            "Warren G was on the " + Noun + ", trying to consume  \n" +
+                                            "Some skirts for the " + timeOfDay + ", so I can get some funk \n" +
+                                            "Just rollin' in my ride, chillin' all alone  \n" +
+                                            "Just hit the Eastside of the " + Place + " \n" +
+                                            "On a mission trying to find Mr.Warren G.  \n";
+
+            Console.WriteLine(madLibRegulators);
+
+            string Regulators = "Here are the real lyrics-- Regulate by Nate Dogg and Warren G \n" +
+                                            "Regulators, mount up... \n " +
+                                            "It was a clear black night, a clear white moon. \n" +
+                                            "Warren G was on the street, trying to consume  \n" +
+                                            "Some skirts for the eve so I can get some funk \n" +
+                                            "Just rollin' in my ride, chillin' all alone  \n" +
+                                            "Just hit the Eastside of the  LBC  \n" +
+                                            "On a mission trying to find Mr.Warren G.  \n";
+
+            Console.WriteLine(Regulators);
+
+        }
+    }
+}
---
+++ MadLib/TurnBackTime.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace MadLib
+{
+    public class TurnBackTime
+    {
+        public TurnBackTime()
+        {
+            Console.WriteLine("Please give me a verb:  ");
+            string Verb = Console.ReadLine();
+
+            Console.WriteLine("Please give me a plural noun:  ");
+            string pluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Please give me a noun:  ");
+            string Noun = Console.ReadLine();
+
+            Console.WriteLine("Please give me a verb:  ");
+            string Verb2 = Console.ReadLine();
+
+            Console.WriteLine("Please give me a verb:  ");
+            string Verb3 = Console.ReadLine();
+
+            Console.WriteLine("Please give me a plural noun:  ");
+            string pluralNoun2 = Console.ReadLine();
+
+
+
+            string timeMadLib = "If I could " + Verb + " back time \n" +
+                                              "If I could find a way \n" +
+                                              "I'd take back those " + pluralNoun + " that've hurt you and you'd stay \n" +
+                                              "I don't know why I did the things I did \n" +
+                                              "I don't know why I said the things I said \n" +
+                                              "Pride's like a knife, it can cut deep inside \n" +
+                                              Noun + " are like weapons, they " + Verb2 + " sometimes \n" +
+                                              "I didn't really mean to " + Verb3 + " you \n" +
+                                              "I didn't wanna see you go \n" +
+                                              "I know I made you cry, but baby \n" +
+                                              "If I could turn back time, if I could find a way \n" +
+                                              "I'd take back those " + pluralNoun2 + " that've hurt you, you'd stay \n" +
+                                              "If I could reach the stars, I'd give 'em all to you \n" +
+                                              "Then you'd love me, love me, like you used to do \n" +
+                                              "If I could turn back time \n";
+
+
+            Console.WriteLine(timeMadLib);
+
+            string turnBackTime = "If I could turn back time \n" +
+                                                  "If I could find a way \n" +
+                                                  "I'd take back those words that've hurt you and you'd stay \n" +
+                                                  "I don't know why I did the things I did \n" +
+                                                  "I don't know why I said the things I said \n" +
+                                                  "Pride's like a knife, it can cut deep inside \n" +
+                                                  "Words are like weapons, they wound sometimes \n" +
+                                                  "I didn't really mean to hurt you \n" +
+                                                  "I didn't wanna see you go \n" +
+                                                  "I know I made you cry, but baby \n" +
+                                                  "If I could turn back time, if I could find a way \n" +
+                                                  "I'd take back those words that've hurt you, you'd stay \n" +
+                                                  "If I could reach the stars, I'd give 'em all to you \n" +
+                                                  "Then you'd love me, love me, like you used to do \n" +
+                                                  "If I could turn back time \n";
+
+
+            Console.WriteLine(turnBackTime);
+
+        }
+    }
+}
